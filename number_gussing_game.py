90c9064a1a520f8edac2ebf1ebf31109e6b478a5
Badal3850 <63769318+Badal3850@users.noreply.github.com>
2021-10-17 20:53:35 +0530

    changed the main code

--- number_gussing_game.py
+++ number_gussing_game.py
@@ -1,24 +1,20 @@
 import random
 print("HEY! Let's play a game.\n Try to guess the Number.\n It can be anything between 1 to 100")
 print("Menu:\n's' to start the game \n'q' to quit the game")
-sstart=input()
-if(sstart=='s'):
-    n=random.randint(0, 100)
-    while(True):
-        try:
-            a= input("Enter a number: ")
-            if(a=='q'):
-                break
-            a=int(a)
-            if(a>n):
-                print("Try Entering a small number")
-            elif(a<n):
-                print("Try Entering a greater number")
-            else:
-                print("wahoooo! You did it @DUDE@")
-        except Exception as e:
-            print(e)
-else:
-    print("Invalid Choice")
-    exit()
-print("Thanks for playing")
+nummber=random.randint(0, 100)
+total_no_of_guess=5
+gussed_number=0
+while(gussed_number!=nummber):
+    total_no_of_guess -=1
+    gussed_number=int(input("Enter a number: "))
+    if(total_no_of_guess==0):
+        print("YOU LOST")
+        exit()
+    else:
+        if(gussed_number>nummber):
+            print("Try entering a smaller Number")
+        elif(gussed_number<nummber):
+            print("Try entering larger number")
+        else:
+            print("Congratulation !! YOU WON")
+    print(f"Number of guess left{total_no_of_guess}: ")
