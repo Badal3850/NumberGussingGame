aa49f088917c0e011c8c1d833ce906d91d547520
BoT <63769318+Badal3850@users.noreply.github.com>
2022-12-06 12:26:05 +0000

    Added if else for menu choice

--- number_gussing_game.py
+++ number_gussing_game.py
@@ -1,24 +1,24 @@
 import random
 print("HEY! Let's play a game.\n Try to guess the Number.\n It can be anything between 1 to 100")
 print("Menu:\n's' to start the game \n'q' to quit the game")
-menu_input=input("")
+menu_input = input("")
 if menu_input == 's':
-    nummber=random.randint(0, 100)
-    totalNoOfGuess=5
-    gussed_number=0
-    while(gussed_number!=nummber):
-        totalNoOfGuess -=1
-        gussed_number=int(input("Enter a number: "))
-        if(totalNoOfGuess==0):
+    nummber = random.randint(0, 100)
+    TOTAL_NUMBER_OF_GUESS = 5
+    GUESSED_NUMBER = 0
+    while GUESSED_NUMBER != nummber:
+        TOTAL_NUMBER_OF_GUESS -= 1
+        GUESSED_NUMBER = int(input("Enter a number: "))
+        if TOTAL_NUMBER_OF_GUESS == 0:
             print("YOU LOST")
             exit()
         else:
-            if(gussed_number>nummber):
+            if GUESSED_NUMBER > nummber:
                 print("Try entering a smaller Number")
-            elif(gussed_number<nummber):
+            elif GUESSED_NUMBER < nummber:
                 print("Try entering larger number")
             else:
                 print("Congratulation !! YOU WON")
-        print(f"Number of guess left{totalNoOfGuess}: ")
+        print(f"Number of guess left{TOTAL_NUMBER_OF_GUESS}: ")
 elif menu_input == 'q':
     print("Have a good day ! See you Soon")
