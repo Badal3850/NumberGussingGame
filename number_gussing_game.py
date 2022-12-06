b949850b4be95cb3b61e268376211fbe9f2facd3
BoT <63769318+Badal3850@users.noreply.github.com>
2022-12-06 11:54:35 +0000

    fixed menu user input

--- number_gussing_game.py
+++ number_gussing_game.py
@@ -3,20 +3,22 @@ print("HEY! Let's play a game.\n Try to guess the Number.\n It can be anything b
 print("Menu:\n's' to start the game \n'q' to quit the game")
 menu_input=input("")
 if menu_input == 's':
-nummber=random.randint(0, 100)
-total_no_of_guess=5
-gussed_number=0
-while(gussed_number!=nummber):
-    total_no_of_guess -=1
-    gussed_number=int(input("Enter a number: "))
-    if(total_no_of_guess==0):
-        print("YOU LOST")
-        exit()
-    else:
-        if(gussed_number>nummber):
-            print("Try entering a smaller Number")
-        elif(gussed_number<nummber):
-            print("Try entering larger number")
+    nummber=random.randint(0, 100)
+    totalNoOfGuess=5
+    gussed_number=0
+    while(gussed_number!=nummber):
+        totalNoOfGuess -=1
+        gussed_number=int(input("Enter a number: "))
+        if(totalNoOfGuess==0):
+            print("YOU LOST")
+            exit()
         else:
-            print("Congratulation !! YOU WON")
-    print(f"Number of guess left{total_no_of_guess}: ")
+            if(gussed_number>nummber):
+                print("Try entering a smaller Number")
+            elif(gussed_number<nummber):
+                print("Try entering larger number")
+            else:
+                print("Congratulation !! YOU WON")
+        print(f"Number of guess left{totalNoOfGuess}: ")
+elif menu_input == 'q':
+    print("Have a good day ! See you Soon")
