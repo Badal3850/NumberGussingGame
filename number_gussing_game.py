10b3bc997e88f710959c0b822e6084d8033a1ebe
BoT <63769318+Badal3850@users.noreply.github.com>
2022-12-06 11:31:21 +0000

    fixed menu input from the user

--- number_gussing_game.py
+++ number_gussing_game.py
@@ -1,6 +1,7 @@
 import random
 print("HEY! Let's play a game.\n Try to guess the Number.\n It can be anything between 1 to 100")
 print("Menu:\n's' to start the game \n'q' to quit the game")
+menu_input=input("")
 nummber=random.randint(0, 100)
 total_no_of_guess=5
 gussed_number=0
